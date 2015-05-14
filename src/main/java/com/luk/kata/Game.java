cd2f49b76b7efb3b479b28a5417d76ef16907626
Lukasz Tracz <kapicel20wp.pl>
2015-05-14 17:53:37 +0200

    Test 1 green

---
+++ src/main/java/com/luk/kata/Game.java
@@ -0,0 +1,12 @@
+package com.luk.kata;
+
+public class Game {
+	 
+	public void roll(int points){
+		
+	}
+	
+	public int score(){
+		return 0;
+	}
+}
--- src/test/java/com/luk/kata/BowlingGameKataTest.java
+++ src/test/java/com/luk/kata/BowlingGameKataTest.java
@@ -2,6 +2,9 @@ package com.luk.kata;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
 public class BowlingGameKataTest {
 	
 	@Test
@@ -9,7 +12,7 @@ public class BowlingGameKataTest {
 		Game g = new Game();
 		for(int i =0; i<20; i++){
 			g.roll(0);
-			assertThat( g.score , 0 );
+			assertThat( g.score() , is(0) );
 		}
 	
 	}
